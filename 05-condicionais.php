3519a536f79d4fc8a6236427ca91e8d3e1482e6c
tiagotecinternet2026 <tiago.tecinternet2026@outlook.com>
2026-08-04 09:05:52 -0300

    Exemplo condicional composta

--- 05-condicionais.php
+++ 05-condicionais.php
@@ -35,6 +35,23 @@
     endif;
 ?>
 
+    <hr>
+
+    <h2>Condicional COMPOSTA: <code>if/else</code></h2>
+<?php  
+$produto = "Ultrabook";
+$qtdEmEstoque = 30; // o que temos a disposição
+$qtdCritica = 5; // mínimo necessário no estoque
+?>
+    <h3><?= $produto ?></h3>
+    <p><b>Quantidade em estoque: </b><?= $qtdEmEstoque ?></p>
+<?php  
+    if ($qtdEmEstoque < $qtdCritica) {
+        echo "<p class=\"comprar\">É necessário comprar/repor</p>";
+    } else {
+        echo "<p class=\"normal\">Estoque normal.</p>";
+    }
+?>
 
 
 
