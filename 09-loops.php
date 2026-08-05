147bf4e9fe28b5258819030b42b9a88f9a23ab1e
tiagotecinternet2026 <tiago.tecinternet2026@outlook.com>
2026-08-05 11:57:18 -0300

    Loops do/while e for

--- 09-loops.php
+++ 09-loops.php
@@ -21,5 +21,38 @@ while($i <= 5): // ou {
     $i++;
 endwhile; // ou }
 ?>
+
+    <hr>
+
+    <h2>do/while (faça/enquanto)</h2>
+    <p>Executa ações pelo menos <b>uma vez</b> e, se a condição for verdadeira, continua executando outras vezes.</p>
+<?php  
+$j = 1;
+do {
+?>
+    <div>
+        <h3>Título qualquer...</h3>
+        <p>Bloco <?= $j ?></p>
+    </div>
+<?php  
+    $j++;
+} while ($j <= 3)
+?>
+
+    <hr>
+
+    <h2>for (para)</h2>
+    <p>Executa ações de acordo com uma <b>quantidade
+    determinada de vezes</b>. </p>
+
+<section>
+    <h3>FAQ (Perguntas Frequentes)</h3>
+<?php for($i = 1; $i <= 5; $i++){ // ou : ?>
+    <details>
+        <summary>Pergunta <?= $i ?></summary>
+        <p>Resposta <?= $i ?></p>
+    </details>
+<?php } // ou endfor; ?>        
+</section>
 </body>
 </html>
