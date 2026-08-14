1ea550a46296da79ea3d97fc6252756ae1ba82fc
tiagotecinternet2026 <tiago.tecinternet2026@outlook.com>
2026-08-14 11:50:36 -0300

    strip_tags, trim, nl2br

--- 12-funcoes-nativas-para-textos.php
+++ 12-funcoes-nativas-para-textos.php
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
 <body>
-    <div class="container">
+    <div class="">
         <h1>Funções nativas para texto</h1>
         <hr>
 
@@ -44,6 +44,49 @@ $fraseCensurada = str_ireplace(
         <p><mark>Frase original: <?= $frase ?></mark></p>
         <p>Frase com substituição de palavra: <?= $fraseComSubstituicaoDePalavra ?></p>
         <p>Frase censurada: <?= $fraseCensurada ?></p>
+
+        <h2>strip_tags()</h2>
+        <?php  
+        $codigoHTML = "<h3>HTML5 - <a href='http://sp.senac.br'>Senac</a> </h3>";
+        $textoSemTags = strip_tags($codigoHTML);
+        ?>
+
+        <div>
+            <?= $codigoHTML ?>
+            <?= $textoSemTags ?>
+        </div>
+
+        <h2>trim()</h2>
+        <?php  
+        $textoComEspacosAdicionais = "  Curso de PHP   "; // formulário
+        $textoSemEspacos = trim($textoComEspacosAdicionais);
+
+        $textoComQuebras = "\n\n Texto qualquer e etc e tal"; // API
+        $textoSemQuebras = trim($textoComQuebras);
+        ?>
+        <pre><?php var_dump($textoComEspacosAdicionais) ?></pre>
+        <pre><?php var_dump($textoSemEspacos) ?></pre>
+        <br><br>
+        <pre><?php var_dump($textoComQuebras) ?></pre>
+        <pre><?php var_dump($textoSemQuebras) ?></pre>
+
+        <h2>nl2br()</h2> <!-- new line to break (br) -->
+        <?php  
+        $textoContendoQuebras = "A vida é feita de escolhas \n e a cada decisão, uma nova jornada começa. \n Às vezes, o caminho parece claro, \n mas em outras, a névoa da dúvida nos envolve. \n Mesmo assim, é importante seguir em frente. \n A coragem está em continuar, \n mesmo quando as respostas não são evidentes. \n Aprendemos com os erros e acertos, \n e cada experiência nos molda para o que está por vir. \n O segredo é nunca parar de caminhar.";
+        ?>
+        <p><?= $textoContendoQuebras ?></p>
+        <p><?= nl2br($textoContendoQuebras) ?></p>
+        <!-- a função pega as "quebras" com \n e transforma em <br> para o HTML -->
+
+        <h2>explode()</h2>
+        <?php  
+        $linguagens = "HTML, CSS, JavaScript, PHP, SQL, Python, C#, Java";
+        $arrayLinguagens = explode(", ", $linguagens);
+        ?>
+        <pre><?php var_dump($linguagens) ?></pre>
+        <pre><?php var_dump($arrayLinguagens) ?></pre>
+
+        
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
