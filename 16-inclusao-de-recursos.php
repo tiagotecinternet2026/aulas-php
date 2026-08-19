1943f19e0de0f3b15e260ba6ff3212ddd6aeb70e
tiagotecinternet2026 <tiago.tecinternet2026@outlook.com>
2026-08-19 11:05:58 -0300

    Uso do include para fragmentos de página/conteúdo

--- 16-inclusao-de-recursos.php
+++ 16-inclusao-de-recursos.php
@@ -19,6 +19,18 @@
     <p>Estamos estudando no <?= ESCOLA ?> fazendo o curso <?= $curso ?>.</p>
     <p>Para fazer este curso o aluno deve ser maior idade.</p>
     <p>Como você <?= ALUNO ?> tem 20 anos, você é <?= verificarIdade(20) ?></p>
+
+    <h2>Exemplo de carregamento de conteúdo parcial/fragmento</h2>
+    <section>
+        <h3>Título qualquer</h3>
+        <?php include "textos.php"; ?>
+        <ul>
+            <?php foreach($tecnologias as $tecnologia): ?>
+                <li><?= $tecnologia ?></li>
+            <?php endforeach; ?>
+        </ul>
+    </section>
+
 </div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>    
---
+++ textos.php
@@ -0,0 +1,8 @@
+<!-- textos.php -->
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam at nesciunt quaerat numquam optio in dolor! Cupiditate, ut eaque eligendi aliquam tempora fugit fugiat, aut nihil sapiente facere ab quam.</p>
+
+<?php include "lista.html"; ?>
+
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam, accusantium.</p>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem non tempora aspernatur earum natus molestiae veritatis, vitae enim quod quas!</p>
