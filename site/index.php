9fbb7b2ecaab5d85443ea5a3e7c894250a07e53f
tiagotecinternet2026 <tiago.tecinternet2026@outlook.com>
2026-08-19 11:23:52 -0300

    Cria página index

---
+++ site/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site usando PHP</title>
+</head>
+<body>
+    <header>
+        <h1>Site com PHP</h1>
+        <nav>
+            <a href="index.php">Home</a>
+            <a href="cursos.php">Cursos</a>
+            <a href="duvidas.php">Dúvidas</a>
+        </nav>
+    </header>
+
+    <main>
+        <h2>Bem-vindo ao site de exemplo</h2>
+        <p>Esta é a primeira página do nosso site</p>
+    </main>
+
+    <footer>
+        <p>Site criado usando PHP</p>
+    </footer>
+</body>
+</html>
