43c2a87e79c9c047fd915a14885b6b285bd4a3cf
tiagotecinternet2026 <tiago.tecinternet2026@outlook.com>
2026-08-20 08:47:11 -0300

    Adiciona Bootstrap

--- site/includes/cabecalho.php
+++ site/includes/cabecalho.php
@@ -8,9 +8,10 @@ const BASE = "/site/";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Site usando PHP</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
 <body>
-    <header>
+    <header class="text-center">
         <h1>Site com PHP</h1>
         <nav>
             <a href="<?= BASE ?>index.php">Home</a>
@@ -21,4 +22,4 @@ const BASE = "/site/";
         </nav>
     </header>
 
-    <main>
+    <main class="container">
--- site/includes/rodape.php
+++ site/includes/rodape.php
@@ -1,7 +1,10 @@
 </main>
 
-    <footer>
+    <footer class="text-center">
         <p>Site criado usando PHP</p>
     </footer>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+
 </body>
 </html>
