2f9e8d6f3c51d072144b56210c7283e9aaf4364e
tiagotecinternet2026 <tiago.tecinternet2026@outlook.com>
2026-08-20 11:56:10 -0300

    Envia e captura dados

--- processamento.php
+++ processamento.php
@@ -7,10 +7,40 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
 <body>
-    <div class="container">
-        <h1>Recebimento e processamento dos dados</h1>
-        <hr>
-    </div>
+<div class="container">
+    <h1>Recebimento e processamento dos dados</h1>
+    <hr>
+
+<?php 
+// Capturando os dados de cada campo
+$nome = $_POST["nome"];
+$email = $_POST["email"];
+$idade = $_POST["idade"];
+$mensagem = $_POST["mensagem"];
+
+/* Operador ?? -> coalescência nula
+Caso nenhum interesse seja selecionado, 
+a variável guardará um array vazio */
+$interesses = $_POST["interesses"] ?? []; 
+
+// Caso nenhuma opção seja selecionada, o valor "nao" fica como padrão
+$informativos = $_POST["informativos"] ?? "nao";
+?>   
+    <h2>Dados recebidos</h2>
+    <p>Nome: <?= $nome ?></p>
+    <p>E-mail: <?= $email ?></p>
+    <p>Idade: <?= $idade ?> anos</p>
+    <p>Mensagem: <?= $mensagem ?> </p>
+    
+    <?php if(!empty($interesses)): ?>
+    <p>Interesses: <?= implode(", ", $interesses) ?></p>
+    <?php endif; ?>
+
+    <p>Informativos: 
+        <?= $informativos === 'sim' ? "Sim" : "Não" ?>
+    </p>
+
+</div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
