dc6dfaa3ce976f9644c5953dfe723d4bd953793c
tiagotecinternet2026 <tiago.tecinternet2026@outlook.com>
2026-08-20 08:56:35 -0300

    Adiciona CSS personalizado

--- site/includes/cabecalho.php
+++ site/includes/cabecalho.php
@@ -9,6 +9,7 @@ const BASE = "/site/";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Site usando PHP</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?= BASE ?>css/estilos.css">
 </head>
 <body>
     <header class="text-center">
--- site/index.php
+++ site/index.php
@@ -1,7 +1,7 @@
 <?php require "includes/cabecalho.php" ?>
     
     <h2>Bem-vindo ao site de exemplo</h2>
-    <p>Esta é a primeira página do nosso site</p>
+    <p>Esta é a <b>primeira página</b> do nosso site</p>
 
 <?php require "includes/rodape.php" ?>
     
