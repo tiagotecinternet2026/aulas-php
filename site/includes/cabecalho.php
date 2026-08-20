9f855f6a6297df990b9b588df014dd58994c3889
tiagotecinternet2026 <tiago.tecinternet2026@outlook.com>
2026-08-20 09:24:40 -0300

    Aplica títulos personalizados

--- site/includes/cabecalho.php
+++ site/includes/cabecalho.php
@@ -1,13 +1,24 @@
 <?php 
 // Definindo um caminho de base/referência para os links
 const BASE = "/site/";
+
+// Detectar/guardar o nome da página aberta pelo usuário
+$pagina = basename($_SERVER['PHP_SELF']);
+
+switch($pagina){
+    case 'index.php': $titulo = "Página Inicial"; break;
+    case 'cursos.php': $titulo = "Cursos"; break;
+    case 'duvidas.php': $titulo = "Dúvidas"; break;
+    case 'planos.php': $titulo = "Planos"; break;
+    default: $titulo = "Consultoria"; break;
+}
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Site usando PHP</title>
+    <title> <?= $titulo ?> - Site usando PHP </title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <link rel="stylesheet" href="<?= BASE ?>css/estilos.css">
 </head>
